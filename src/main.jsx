a16f70799cdbd957dcb135a1c68f229cce4b69f0
rozer4heros <rozer4heros@gmail.com>
2026-06-29 16:46:57 +0900

    feat: update project configuration and dependencies for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 // import { StrictMode } from "react";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   // </StrictMode>,
 );
